18aa8e5128a2cf41fbd1750a14e97be825a74716
Dominik Dosoudil <dosoudil@insoft.cz>
2017-12-24 15:17:42 +0100

    Condition for server/client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+use std::env;
+
+fn client() {
+	println!("clienting");
+}
+
+fn server() {
+	println!("starting server");
+}
+
+fn help() {
+	println!("Usage:\nmain (server|client)");
+}
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+	match args.len() {
+		2 => {
+			match &args[1][..] {
+				"server" => server(),
+				"client" => client(),
+				_ => help(),
+			}
+		},
+		_ => {
+			help();
+		}
+	}
 }
